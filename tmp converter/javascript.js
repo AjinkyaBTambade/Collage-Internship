b958129140f8a59f40c457c628bf9404377ad5a0
Ajinkya <ajinkyatambade143@gmail.com>
2024-03-07 11:52:06 +0530

    Modified  files added

--- tmp converter/javascript.js
+++ tmp converter/javascript.js
@@ -1,24 +1,26 @@
-const convertCelsius =() =>{
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('convertCelsius').addEventListener('click', convertCelsius);
+    document.getElementById('convertFahrenheit').addEventListener('click', convertFahrenheit);
+    document.getElementById('reset').addEventListener('click', reset);
+});
+
+function convertCelsius() {
     var fahrenheit = parseFloat(document.querySelector("#fahrenheit").value);
-
-    var celsiusResult = (fahrenheit-32)/1.8;
+    var celsiusResult = (fahrenheit - 32) / 1.8;
     var b = celsiusResult.toFixed(2);
-
     document.querySelector("#fahrenheit").value = fahrenheit;
-    document.querySelector("#celsius").value =b;
+    document.querySelector("#celsius").value = b;
 }
 
-const convertFahrenheit =() =>{
+function convertFahrenheit() {
     var celsius = parseFloat(document.querySelector("#celsius").value);
-
-    var fahrenheitResult = (1.8 * celsius)+32;
+    var fahrenheitResult = (1.8 * celsius) + 32;
     var a = fahrenheitResult.toFixed(2);
-
     document.querySelector("#celsius").value = celsius;
     document.querySelector("#fahrenheit").value = a;
 }
 
-const reset = () =>{
-    document.querySelector("#fahrenheit").value = " ";
-    document.querySelector("#celsius").value = " ";  
+function reset() {
+    document.querySelector("#fahrenheit").value = "";
+    document.querySelector("#celsius").value = "";
 }
